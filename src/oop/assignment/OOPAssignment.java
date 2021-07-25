11498d1d25c9e840aba23095eb172355eadfd444
Yuetfoong0817 <leeyf-wm19@student.tarc.edu.my>
2021-07-25 16:31:17 +0800

    yf here 2507

--- src/oop/assignment/OOPAssignment.java
+++ src/oop/assignment/OOPAssignment.java
@@ -17,6 +17,7 @@ public class OOPAssignment {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Testing");
+        System.out.println("Hi Yf here");
     }
     
 }
