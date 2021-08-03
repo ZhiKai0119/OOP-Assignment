042b887181daebbff5ac552cd958659f90887486
ZhiKai0119 <chongzk-wm19@student.tarc.edu.my>
2021-08-03 14:07:15 +0800

    Create by ZK

---
+++ src/connection/DatabaseConnection.java
@@ -0,0 +1,32 @@
+package connection;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private Connection connection;
+    
+    public static DatabaseConnection getInstance() {
+        if(instance == null){
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+    
+    private DatabaseConnection() {
+        
+    }
+    
+    public void connectToDatabase() throws SQLException, ClassNotFoundException {
+        final String URL = "jdbc:mysql://localhost:3306/idfc";
+        final String USER = "root";
+        //final String PWD = "mysql@123";
+        final String PWD = "mysql@123";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(URL, USER, PWD);
+    }
+    
+    public Connection getConnection() {
+        return connection;
+    }
+}
