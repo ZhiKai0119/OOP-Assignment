cea101d779f3bae338b4df74737f03129ebfb281
ZhiKai0119 <chongzk-wm19@student.tarc.edu.my>
2021-08-14 18:33:58 +0800

    ZK 0814

--- src/connection/DatabaseConnection.java
+++ src/connection/DatabaseConnection.java
@@ -20,8 +20,8 @@ public class DatabaseConnection {
     public void connectToDatabase() throws SQLException, ClassNotFoundException {
         final String URL = "jdbc:mysql://localhost:3306/idfc";
         final String USER = "root";
-        final String PWD = "mysql@123";
-        //final String PWD = "1234";
+        //final String PWD = "mysql@123";
+        final String PWD = "1234";
         Class.forName("com.mysql.cj.jdbc.Driver");
         connection = DriverManager.getConnection(URL, USER, PWD);
     }
